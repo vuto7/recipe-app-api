1bc22a9c3d0c3fa5be88c4954a8c06208d8366b6
Malama.Kangwa.SZI <58172771+MalamaKangwa@users.noreply.github.com>
2024-06-26 19:24:18 +0200

    added image to recipe model

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -21,8 +21,8 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
     """ Manage ingredients in the database """
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
-    
-    
+
+
 class RecipeViewSet(viewsets.ModelViewSet):
     #serializer_class = serializers.RecipeDetailSerializer
     serializer_class = serializers.RecipeSerializer
@@ -30,12 +30,24 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_class = [IsAuthenticated]
 
+    def _params_to_ints(self, qs):
+        """Convert a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        """Retrieve the recipes for the authenticated user"""
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in = ingredient_ids)
+
+        return queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
         if self.action == 'list':
-            return serializers.RecipeSerializer
+            return serializers.RecipeSerializer 
 
         return self.serializer_class
 
